a77d1fe8034083c89eb911f81a279b71b3b7b2ef
Jade Jhagru <j.jhagru@hotmail.com>
2021-06-30 16:39:36 -0400

    navbar css

--- myportfolio/src/components/my-navbar/my-navbar.component.jsx
+++ myportfolio/src/components/my-navbar/my-navbar.component.jsx
@@ -1,27 +1,30 @@
-import React from 'react'
-import MyNavbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Logo from "../../assets/icons/logo.png";
+import "../my-navbar/my-navbar.style.css";
 
-const Navbar = () => {
+const MyNavbar = () => {
   return (
     <>
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar fixed="top" collapseOnSelect expand="md" variant="dark" className="animate-navbar nav-theme justify-content-between">
+  <Navbar.Brand href="#home">
+    <img className="logo" src={Logo} alt="My Portfolio Logo" />
+  </Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    <Nav className="ml-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#about">About</Nav.Link>
+      <Nav.Link href="#skills">Skills</Nav.Link>
+      <Nav.Link href="#experience">Experience</Nav.Link>
+      <Nav.Link href="#projects">Projects</Nav.Link>
+      <Nav.Link href="#contact">Contact</Nav.Link>
+      
     </Nav>
-    <Nav>
-      <Nav.Link href="#deets">More deets</Nav.Link>
-      <Nav.Link eventKey={2} href="#memes">
-        Dank memes
-      </Nav.Link>
-    </Nav>
+   
   </Navbar.Collapse>
 </Navbar>
-
     </>
   )
 }
