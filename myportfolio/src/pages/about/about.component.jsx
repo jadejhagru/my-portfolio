783170b545e78bef051cbb3103aa4fe210d004bc
Jade Jhagru <j.jhagru@hotmail.com>
2021-07-06 20:00:45 -0400

    about me intro

---
+++ myportfolio/src/pages/about/about.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Row from 'react-bootstrap/Row'
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+
+const About = () => {
+    return (
+        <div id="about">
+            <h1>About Me</h1>
+            <Container>
+                <Row>
+                    <Col xs={12} md={6}>
+                        
+                    </Col>
+                </Row>
+            </Container>
+
+        </div>
+    )
+}
+
+export default About  
