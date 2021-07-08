514348a0311897c8be2292a19a314b70720707e0
Jade Jhagru <j.jhagru@hotmail.com>
2021-07-08 16:09:20 -0400

    projects component added & npm timeline

---
+++ myportfolio/src/components/projects-timeline/projects-timeline.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Timeline, Events, UrlButton, ImageEvent } from "@merc/react-timeline";
+
+const ProjectTimeline = () => {
+    return (
+        <div id="projects">
+
+        </div>
+    )
+}
+
+export default ProjectTimeline
